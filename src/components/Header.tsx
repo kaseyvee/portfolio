b0e559f0d02a1543c49899062d95de2ec354a75b
kethnal <kaseyvaldez@hotmail.com>
2023-01-25 05:27:37 -0800

    Create variables for scss, start header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div>
+      <h1>kasey valdez</h1>
+      <h2>full-stack developer</h2>
+      <div className="links">
+      <i className="fa-brands fa-github-alt"></i>
+      <i className="fa-brands fa-linkedin-in"></i>
+      <i className="fa-solid fa-envelope"></i>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
