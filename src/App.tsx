fe3fa4b0a639688c952f8018ecfe56250dd74938
kethnal <kaseyvaldez@hotmail.com>
2023-01-25 05:50:20 -0800

    Create style about component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
+import About from './components/About';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,16 @@
 function About() {
   return (
-    <div>
-      
+    <div className="About">
+      <h2>about</h2>
+      <div>
+        <h2>projects</h2>
+        <div className="projects-dropdown">
+          <h3>completed</h3>
+          <h3>in progress</h3>
+        </div>
+      </div>
+      <h2>contact</h2>
+      <h2>resume</h2>
     </div>
   );
 }
