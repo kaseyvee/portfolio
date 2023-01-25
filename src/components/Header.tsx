6e10617eed908a0446aa0aa01e551f48f0d4a111
kethnal <kaseyvaldez@hotmail.com>
2023-01-25 05:41:00 -0800

    Add links to header icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 function Header() {
   return (
-    <div>
+    <div className="Header">
       <h1>kasey valdez</h1>
       <h2>full-stack developer</h2>
       <div className="links">
-      <i className="fa-brands fa-github-alt"></i>
-      <i className="fa-brands fa-linkedin-in"></i>
-      <i className="fa-solid fa-envelope"></i>
+      <a href="https://github.com/kaseyvee" target="_blank" rel="noreferrer noopener"><i className="fa-brands fa-github-alt"></i></a>
+      <a href="https://www.linkedin.com/in/kasey-valdez/" target="_blank" rel="noreferrer noopener"><i className="fa-brands fa-linkedin-in"></i></a>
+      <a href="" target="_blank" rel="noreferrer noopener"><i className="fa-solid fa-envelope"></i></a>
       </div>
     </div>
   );
