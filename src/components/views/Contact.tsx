1b299cbede4060e527a6c8fe8ff1c226cb677049
kethnal <kaseyvaldez@hotmail.com>
2023-01-27 05:20:08 -0800

    Implement contact form

--- src/components/views/Contact.tsx
+++ src/components/views/Contact.tsx
@@ -1,14 +1,47 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Badge } from "../Badge";
+
+declare namespace Email {
+  type Attachment =
+    | {
+        name: string;
+        path: string;
+      }
+    | {
+        name: string;
+        data: string;
+      };
+
+  interface EmailData {
+    SecureToken: string;
+    To: string | string[];
+    From: string;
+    Subject: string;
+    Body: string;
+  }
+
+  function send(email: EmailData): Promise<string>;
+}
 
 const Contact: React.FC<{ loading: boolean }> = (props) => {
   const [emailSent, setEmailSent] = useState(false);
+  const email = useRef<HTMLInputElement>(null);
+  const subject = useRef<HTMLInputElement>(null);
+  const body = useRef<HTMLTextAreaElement>(null);
 
   function handleSendEmail() {
+    Email.send({
+      SecureToken: "03ec791a-aedb-42ce-a112-63a6e149a1f2",
+      To: 'kaseyvaldez98@gmail.com',
+      From: `${email}`,
+      Subject: `${subject}`,
+      Body: `${body}`
+  }).then(() => {
     setEmailSent(true);
     setTimeout(() => {
       setEmailSent(false);
     }, 3000);
+  });
   }
 
   return (
@@ -18,9 +51,9 @@ const Contact: React.FC<{ loading: boolean }> = (props) => {
           <>
           <h2>coffee chat? - <a href="mailto:kaseyvaldez98@gmail.com?subject=Hello Cool Person!">kaseyvaldez98@gmail.com</a></h2>
           <form>
-            <input placeholder="name"/>
-            <input placeholder="email"/>
-            <textarea placeholder="hey kasey! what do you think of my new song idea- it goes: oooo wah ah ah ah yayayaya heyooo *bag pipes*"/>
+            <input ref={email} placeholder="email"/>
+            <input ref={subject} placeholder="subject"/>
+            <textarea ref={body} placeholder="hey kasey! what do you think of my new song idea- it goes: oooo wah ah ah ah yayayaya heyooo *bag pipes*"/>
           </form>
           <Badge text="send" handleSendEmail={handleSendEmail}/>
         </>}
