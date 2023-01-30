a1f28803e466912c16bbe68fa7596d9785a29821
kethnal <kaseyvaldez@hotmail.com>
2023-01-30 11:54:07 -0800

    hell

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,5 @@
+import { NONAME } from 'dns';
+import { useState } from 'react';
 import { completedProjects, inProgressProjects } from '../database/projects';
 
 interface ProjectList {
@@ -18,39 +20,53 @@ interface Props {
 }
 
 export const Menu: React.FC<Props> = (props) => {
+  const [burger, setBurger] = useState(false);
 
+  function handleToggleBurger() {
+    if (burger) {
+      return setBurger(false);
+    }
+    return setBurger(true);
+  }
+
+  const burgerToggleStyle = {
+    display: 'block'
+  }
   
   return (
     <div className="Menu">
-      <h2
-        onClick={() => props.handleViewToggle("about")}
-        className={`${props.view === "about" && "underline"}`}
-      >about</h2>
-      <div>
+      <i className="fa-solid fa-burger" onClick={handleToggleBurger}></i>
+      <div className="menu-items" style={burger ? burgerToggleStyle : {}}>
         <h2
-          onClick={() => props.handleViewToggle("projects")}
-          className={`${props.view === "projects" && "underline"}`}
-        >projects</h2>
-        <div className="projects-dropdown">
-          {props.view === "projects" &&
-            <>
-              <h3
-                onClick={props.handleViewCompleted}
-                className={`${props.projectType === completedProjects && "highlight"}`}
-              >completed</h3>
-              <h3
-                onClick={props.handleViewInProgress}
-                className={`${props.projectType === inProgressProjects && "highlight"}`}
-              >in progress</h3>
-            </>
-          }
+          onClick={() => props.handleViewToggle("about")}
+          className={`${props.view === "about" && "underline"}`}
+        >about</h2>
+        <div>
+          <h2
+            onClick={() => props.handleViewToggle("projects")}
+            className={`${props.view === "projects" && "underline"}`}
+          >projects</h2>
+          <div className="projects-dropdown">
+            {props.view === "projects" &&
+              <>
+                <h3
+                  onClick={props.handleViewCompleted}
+                  className={`${props.projectType === completedProjects && "highlight"}`}
+                >completed</h3>
+                <h3
+                  onClick={props.handleViewInProgress}
+                  className={`${props.projectType === inProgressProjects && "highlight"}`}
+                >in progress</h3>
+              </>
+            }
+          </div>
         </div>
+        <h2
+          onClick={() => props.handleViewToggle("contact")}
+          className={`${props.view === "contact" && "underline"}`}
+        >contact</h2>
+        <a href="https://resume.creddle.io/resume/hje8hgzfv4a" target="_blank" rel="noreferrer noopener"><h2>resume</h2></a>
       </div>
-      <h2
-        onClick={() => props.handleViewToggle("contact")}
-        className={`${props.view === "contact" && "underline"}`}
-      >contact</h2>
-      <a href="https://resume.creddle.io/resume/hje8hgzfv4a" target="_blank" rel="noreferrer noopener"><h2>resume</h2></a>
     </div>
   );
 }
